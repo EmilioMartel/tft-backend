37521453c0e681544e4e06f20f1ea37e5b2b1f35
Emilio Martel <emilio_martel@outlook.es>
2025-03-10 13:56:11 +0000

    create upload file use case

---
+++ src/application/use-cases/graph/upload-file-use-case.ts
@@ -0,0 +1,14 @@
+import { LinkEntity, NodeEntity } from "../../../domain/entities";
+import { FileService } from "../../../infrastructure/services";
+
+export interface UseCase<T, R> {
+  execute(param: T): R;
+}
+
+export class UploadFileUseCase implements UseCase<Express.Multer.File, Promise<void>> {
+  constructor(private fileService: FileService) {}
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    await this.fileService.saveFile(file);
+  }
+}
