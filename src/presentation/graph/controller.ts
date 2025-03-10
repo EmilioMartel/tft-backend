c948613b37653841f20ae4d7240ff5b95686c071
Emilio Martel <emilio_martel@outlook.es>
2025-03-10 13:57:58 +0000

    add upload endpoint

--- src/presentation/graph/controller.ts
+++ src/presentation/graph/controller.ts
@@ -1,16 +1,16 @@
-
-
-
 import { Request, Response } from "express";
 import { GetNodesUseCase } from "../../application/use-cases";
 import { FileService } from "../../infrastructure/services";
+import { UploadFileUseCase } from "../../application/use-cases/graph/upload-file-use-case";
 
 export class GraphController {
   private getNodesUseCase: GetNodesUseCase;
-
+  private uploadFileUseCase: UploadFileUseCase;
+  
   constructor() {
     const fileService = new FileService();
     this.getNodesUseCase = new GetNodesUseCase(fileService);
+    this.uploadFileUseCase = new UploadFileUseCase(fileService);
   }
 
   public getTodos = (req: Request, res: Response): void => {
@@ -20,6 +20,23 @@ export class GraphController {
     } catch (error) {
       console.error("Error al obtener el grafo:", error);
       res.status(500).send("Error al obtener el grafo.");
+    }
+  };
+
+  public uploadFile = async (req: Request, res: Response): Promise<void> => {
+    if (!req.file) {
+      res.status(400).json({ error: "No se ha subido ningún archivo." });
+      return;
+    }
+
+    try {
+      await this.uploadFileUseCase.execute(req.file);
+      
+      console.log("📁 Archivo recibido:", req.file);
+      res.status(200).json({ message: "Archivo subido correctamente." });
+    } catch (error) {
+      console.error("Error al subir el archivo:", error);
+      res.status(500).json({ error: "Error al subir el archivo." });
     }
   };
 }
--- src/presentation/graph/routes.ts
+++ src/presentation/graph/routes.ts
@@ -1,16 +1,26 @@
 import { Router } from "express";
 import { GraphController } from "./controller";
+import multer from "multer";
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
 export class GraphRoutes {
-
   static get routes(): Router {
     const router = Router();
     const graphController = new GraphController();
 
-    router.get( '/', graphController.getTodos );
+    router.get('/', graphController.getTodos);
+    
+    router.post("/upload", upload.single("file"), graphController.uploadFile);
 
     return router;
   }
-
 }
