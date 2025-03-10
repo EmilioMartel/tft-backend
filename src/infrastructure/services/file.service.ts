ee73742b81a820862fd2834b91da1a0aff1d20b3
Emilio Martel <emilio_martel@outlook.es>
2025-03-10 13:55:44 +0000

    add trycatch to save file

--- src/infrastructure/services/file.service.ts
+++ src/infrastructure/services/file.service.ts
@@ -1,6 +1,26 @@
 import fs from 'fs';
+import path from "path";
 
 export class FileService {
+
+  private uploadPath = path.join(__dirname, "../../../uploads");
+
+  constructor() {
+    this.ensureUploadPath();
+  }
+
+  private async ensureUploadPath(): Promise<void> {
+    if (!fs.existsSync(this.uploadPath)) {
+      fs.promises.mkdir(this.uploadPath);
+    }
+  }
+
+  async saveFile(file: Express.Multer.File): Promise<void> {
+    await this.ensureUploadPath();
+    const filePath = path.join(this.uploadPath, file.originalname);
+    await fs.promises.rename(file.path, filePath);
+  }
+
   readFile(filePath: string) {
     try {
       const rawData = fs.readFileSync(filePath, "utf-8");
