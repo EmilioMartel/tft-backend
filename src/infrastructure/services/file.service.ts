75a92732be1206f913f3fb9463713bbf7377475d
Emilio Martel <emilio_martel@outlook.es>
2025-03-10 15:37:34 +0000

    add custom path to save / load a file

--- src/infrastructure/services/file.service.ts
+++ src/infrastructure/services/file.service.ts
@@ -1,31 +1,54 @@
 import fs from 'fs';
 import path from "path";
+import { envs } from '../../config/envs';
 
 export class FileService {
 
-  private uploadPath = path.join(__dirname, "../../../uploads");
+  private destinationPath = path.join(__dirname, "../../..");
 
-  constructor() {
-    this.ensureUploadPath();
-  }
-
-  private async ensureUploadPath(): Promise<void> {
-    if (!fs.existsSync(this.uploadPath)) {
-      fs.promises.mkdir(this.uploadPath);
-    }
-  }
+  constructor() {}
 
   async saveFile(file: Express.Multer.File): Promise<void> {
-    await this.ensureUploadPath();
-    const filePath = path.join(this.uploadPath, file.originalname);
+    const finalPath = path.join(this.destinationPath, envs.FILE_PATH);
+    if (!fs.existsSync(finalPath)) {
+      fs.promises.mkdir(finalPath, { recursive: true });
+    }
+    
+    const filePath = path.join(finalPath, file.originalname);
     await fs.promises.rename(file.path, filePath);
   }
 
-  readFile(filePath: string) {
+  readFile(filePath: string): any {
+    const finalPath = path.join(this.destinationPath, filePath);
     try {
+       if (!fs.existsSync(finalPath)) {
+        throw new Error("La carpeta 'uploads' no existe.");
+      }
+
+      const files = fs.readdirSync(finalPath);
+
+      if (files.length === 0) {
+        throw new Error("No hay archivos en la carpeta 'uploads'.");
+      }
+
+      const filePath = path.join(finalPath, files[0]);
+
+      const stats = fs.statSync(filePath);
+      if (!stats.isFile()) {
+        throw new Error(`La ruta proporcionada no es un archivo válido: ${filePath}`);
+      }
+
       const rawData = fs.readFileSync(filePath, "utf-8");
-      if (!rawData) throw new Error("El archivo está vacío");
-      return JSON.parse(rawData);
+
+      if (!rawData) {
+        throw new Error("El archivo está vacío.");
+      }
+
+      try {
+        return JSON.parse(rawData);
+      } catch (jsonError) {
+        throw new Error("Error al parsear el archivo JSON.");
+      }
     } catch (error) {
       console.error("Error al leer el archivo:", error);
       throw new Error("No se pudo leer el archivo.");
